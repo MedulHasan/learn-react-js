fde0a982984c1265d0a16bf75360f5849c04cb60
Medul Hasan <medul.cse@gmail.com>
2021-08-10 23:43:14 +0600

    useCallbanc and useMemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useCallback, useState, useMemo } from 'react';
+import Button from './components/Button';
+import ShowCount from './components/ShowCount';
+import Title from './components/Title';
 
-export default class App extends React.Component {
-    render() {
-        return <div>Hello Worls</div>;
-    }
+function App() {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = useCallback(() => {
+        setCount1((prevCount) => prevCount + 1);
+    }, []);
+    const incrementByFive = useCallback(() => {
+        setCount2((prevCount) => prevCount + 5);
+    }, []);
+
+    const isEvenOrOdd = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) i += 1;
+        return count1 % 2 === 0;
+    }, [count1]);
+
+    return (
+        <div className="app">
+            <Title />
+            <ShowCount count={count1} title="Counter 1" />
+            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
+            <Button handleClick={incrementByOne}>Increment by one</Button>
+            <hr />
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment by five</Button>
+        </div>
+    );
 }
+
+export default App;
